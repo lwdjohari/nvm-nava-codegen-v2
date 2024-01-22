9e2976dd7379795c68cf04dae224a91c7c161a1e
lwdjohari <linggawasistha.djohari@outlook.com>
2024-01-22 12:01:09 +0800

    Add project cmake cxx17 with google coding style.

---
+++ inc/nava/utils.h
@@ -0,0 +1,16 @@
+#ifndef NAVA_V2_UTILS_H
+#define NAVA_V2_UTILS_H
+
+namespace nvm {
+namespace nava {
+  
+class Utils {
+ private:
+ public:
+  Utils();
+  ~Utils();
+};
+
+}  // namespace nava
+}  // namespace nvm
+#endif
---
+++ src/nava/utils.cc
@@ -0,0 +1,11 @@
+#include "nava/utils.h"
+
+namespace nvm {
+namespace nava {
+
+Utils::Utils() {}
+
+Utils::~Utils() {}
+}  // namespace nava
+
+}  // namespace nvm
